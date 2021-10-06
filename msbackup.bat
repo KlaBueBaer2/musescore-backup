782fb33314952054332b074d449190fcde69be08
sherlocksoftcollection <78736665+sherlocksoftcollection@users.noreply.github.com>
2021-10-06 22:34:29 +0800

    Update msbackup.bat

--- msbackup.bat
+++ msbackup.bat
@@ -31,6 +31,7 @@ echo.
 echo.------------------------------------------------------------------------------------------------------------------------
 echo.Press E for help.
 choice /c hbrea /n %1
+
 IF ERRORLEVEL ==5 GOTO ABOUT
 IF ERRORLEVEL ==4 GOTO HELP
 IF ERRORLEVEL ==3 GOTO RESTORE
@@ -41,9 +42,9 @@ cls
 echo.                                        (Home) [Backup] (Restore) (hElp) (About)
 echo.------------------------------------------------------------------------------------------------------------------------
 echo.
+echo.Welcome to the backup wizard.  This will guide you on backing up MuseScore.  Currently supports MuseScore 3.
 echo.
-echo.
-echo.
+echo.Press N to continue...
 echo.
 echo.
 echo.
@@ -67,12 +68,73 @@ echo.
 echo.
 echo.------------------------------------------------------------------------------------------------------------------------
 echo.Press H for help.
-choice /c hbrea /n %1
+choice /c hbrean /n %1
+If ERRORLEVEL ==6 GOTO BACKUP2
 IF ERRORLEVEL ==5 GOTO ABOUT
 IF ERRORLEVEL ==4 GOTO HELP
 IF ERRORLEVEL ==3 GOTO RESTORE
 IF ERRORLEVEL ==2 GOTO BACKUP
 IF ERRORLEVEL ==1 GOTO HOME
+:BACKUP2
+cls
+echo.                                        (Home) [Backup] (Restore) (hElp) (About)
+echo.------------------------------------------------------------------------------------------------------------------------
+echo.
+echo.What do you want to exprot the backup as?
+echo.Select the corresponding number.
+echo.
+echo.1. As a folder
+echo.2. As a 7z archive
+echo.3. As a rar archive
+echo.4. As a zip archive
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.------------------------------------------------------------------------------------------------------------------------
+echo.Press H for help.
+choice /c hbrea1234 /n %1
+If ERRORLEVEL ==9 GOTO BACKUPZIP
+If ERRORLEVEL ==8 GOTO BACKUPRAR
+If ERRORLEVEL ==7 GOTO BACKUP7Z
+If ERRORLEVEL ==6 GOTO BACKUPFOLDER
+IF ERRORLEVEL ==5 GOTO ABOUT
+IF ERRORLEVEL ==4 GOTO HELP
+IF ERRORLEVEL ==3 GOTO RESTORE
+IF ERRORLEVEL ==2 GOTO BACKUP
+IF ERRORLEVEL ==1 GOTO HOME
+:BACKUPFOLDER
+cls
+echo.
+echo.Lastly, where do you want to backup to? The contents of the settings folder will be copied into this folder.
+echo.Example directory: C:\myfiles\musescorebackup
+echo.
+echo. Backup directory:
+set /p folderdir=
+echo.
+echo.Configuration complete!
+pause
+cls
+C:
+cd %USERPROFILE%\AppData\Local\MuseScore\MuseScore3
+xcopy /E %USERPROFILE%\AppData\Local\MuseScore\MuseScore3 "%folderdir%"
+echo.========================================================================================================================
+echo.Finished!
+pause
+GOTO HOME
 :RESTORE
 cls
 echo.                                        (Home) (Backup) [Restore] (hElp) (About)
