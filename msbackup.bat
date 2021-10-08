6811fe0b5985bffc3ce26d36dbe1d08fa5a0c160
sherlocksoftcollection <78736665+sherlocksoftcollection@users.noreply.github.com>
2021-10-08 08:59:38 +0800

    Bug fixes and added icon

--- msbackup.bat
+++ msbackup.bat
@@ -1,5 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCqDJGmW+0UiKRZHcAmXL26pBboO7aX57umMt10hWfE2e4DVlL2NL4A=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJGmW+0UiKRZHcCmXL26JBboO7ajZ7umMt10NUfADdZzZ27eFNfBd713hFQ==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
-title MuseScore Backupper v1.0 Developer Nightly Build
+title MuseScore Backupper v1.0 Beta
 set currentdir=%CD%
 set disk=%cd:~0,2%
 :HOME
@@ -389,8 +422,7 @@ echo.
 echo.
 echo.
 echo.   Version Info:
-echo.   MuseScore Backupper v0.1 development nightly build.  Revision no. abcdefg
-echo.
+echo.   MuseScore Backupper v1.0 beta.
 echo.
 echo.
 echo.
